8465cedf44d84c1ebcbc9a3e9d32aa2853362dcd
ShaherAhly <42422914+ShaherAhly@users.noreply.github.com>
2018-11-22 15:52:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,5 +26,25 @@ var channel = "494265034789093386";//ايدي الروم
     client.guilds.get(server).channels.get(channel).send('#daily')
     },86400);
 })
+client.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
 
+  let prefix = "2";
+  let messageArray = message.content.split (" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+
+
+
+
+
+  if(cmd === `${prefix}say`){
+     if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Oh hell noooo oof , You don't have permission biacht");
+    let botmessage = args.join(" ");
+    message.delete().catch();
+  message.channel.send(`${botmessage}`);
+  }
+});
 client.login(process.env.BOT_TOKEN);
